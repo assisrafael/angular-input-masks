3981ac700ef3ec59be54d935e92cefcd7b293957
burakkilic <burakkilic@gmail.com>
2014-12-16 18:33:20 +0200

    Update money symbol
    We can also define currency symbol

--- releases/masks.js
+++ releases/masks.js
@@ -1363,7 +1363,7 @@ angular.module('ui.utils.masks.money', [])
 			link: function (scope, element, attrs, ctrl) {
 				var decimalDelimiter = $locale.NUMBER_FORMATS.DECIMAL_SEP,
 					thousandsDelimiter = $locale.NUMBER_FORMATS.GROUP_SEP,
-					currencySym = $locale.NUMBER_FORMATS.CURRENCY_SYM,
+					currencySym = attrs.uiMoneySymbol,
 					decimals = parseInt(attrs.uiMoneyMask);
 
 				if (!ctrl) {
