401c5a74835ac5ee959905930e2bbf3a125034d2
lsneto <lndro.neto@gmail.com>
2015-03-09 08:40:35 -0300

    add gulp build

--- releases/masks.js
+++ releases/masks.js
@@ -376,4 +376,13 @@ angular.module('ui.utils.masks.br.phone', [])
 	};
 }]);
 
+'use strict';
+
+angular.module('ui.utils.masks', [
+	'ui.utils.masks.br'
+])
+.config(['$logProvider', function($logProvider) {
+	$logProvider.debugEnabled(false);
+}]);
+
 })(angular);
