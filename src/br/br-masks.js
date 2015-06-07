b6582452f1171a59c1954af80100460c9db850ce
Igor Rafael <igor.rafael@gmail.com>
2015-06-07 20:33:09 +0200

    feat(ui-br-boleto-bancario): define a component to parse and format brazilian "boleto bancário"

--- src/br/br-masks.js
+++ src/br/br-masks.js
@@ -2,6 +2,7 @@ var m = angular.module('ui.utils.masks.br', [
 	require('../helpers'),
 	require('./cpf-cnpj/cpf-cnpj')
 ])
+.directive('uiBrBoletoBancarioMask', require('./boleto-bancario/boleto-bancario'))
 .directive('uiBrCepMask', require('./cep/cep'))
 .directive('uiBrIeMask', require('./inscricao-estadual/ie'))
 .directive('uiNfeAccessKeyMask', require('./nfe/nfe'))
