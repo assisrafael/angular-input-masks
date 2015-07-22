1ab78e0b5454cda8fb286581658bf1b43b2a522e
unknown <tiagopereira@CXCRESCER-42.CAIXACRESCER.CORP>
2015-07-22 11:05:36 -0300

    Inserção de Máscara do Tipo "Mês/Ano"
    Insertion of "Month/Year" mask Type

---
+++ src/global/date/month-year.js
@@ -0,0 +1,62 @@
+var moment = require('moment');
+var StringMask = require('string-mask');
+
+function MonthYearMaskDirective($locale) {
+	var dateFormatMapByLocale = {
+		'pt-br': 'MM/YYYY',
+	};
+
+	var dateFormat = dateFormatMapByLocale[$locale.id] || 'YYYY-MM';
+
+	return {
+		restrict: 'A',
+		require: 'ngModel',
+		link: function(scope, element, attrs, ctrl) {
+			var dateMask = new StringMask(dateFormat.replace(/[YM]/g,'0'));
+
+			function formatter(value) {
+				if (ctrl.$isEmpty(value)) {
+					return value;
+				}
+
+				var cleanValue = value;
+				if (typeof value === 'object') {
+					cleanValue = moment(value).format(dateFormat);
+				}
+
+				cleanValue = cleanValue.replace(/[^0-9]/g, '');
+				var formatedValue = dateMask.apply(cleanValue) || '';
+
+				return formatedValue.trim().replace(/[^0-9]$/, '');
+			}
+
+			ctrl.$formatters.push(formatter);
+
+			ctrl.$parsers.push(function parser(value) {
+				if (ctrl.$isEmpty(value)) {
+					return value;
+				}
+
+				var formatedValue = formatter(value);
+
+				if (ctrl.$viewValue !== formatedValue) {
+					ctrl.$setViewValue(formatedValue);
+					ctrl.$render();
+				}
+
+				return moment(formatedValue, dateFormat).toDate();
+			});
+
+			ctrl.$validators.date =	function validator(modelValue, viewValue) {
+				if (ctrl.$isEmpty(modelValue)) {
+					return true;
+				}
+
+				return moment(viewValue, dateFormat).isValid() && viewValue.length === dateFormat.length;
+			};
+		}
+	};
+}
+MonthYearMaskDirective.$inject = ['$locale'];
+
+module.exports = MonthYearMaskDirective;
