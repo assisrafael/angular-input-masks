ebf06cc36681a67821967733909318a9dad77920
Thai Truong <tht2401@gmail.com>
2015-11-06 10:48:48 +0700

    support short/long or both in time-mask

--- src/global/time/time.js
+++ src/global/time/time.js
@@ -5,23 +5,65 @@ module.exports = function TimeMaskDirective() {
 		restrict: 'A',
 		require: 'ngModel',
 		link: function(scope, element, attrs, ctrl) {
-			var timeFormat = '00:00:00';
+			var timeFormat = null;
 
-			if (angular.isDefined(attrs.uiTimeMask) && attrs.uiTimeMask === 'short') {
-				timeFormat = '00:00';
+			if (angular.isDefined(attrs.uiTimeMask) && (attrs.uiTimeMask === 'short' || attrs.uiTimeMask === 'long')) {
+				timeFormat = attrs.uiTimeMask;
 			}
 
-			var formattedValueLength = timeFormat.length;
-			var unformattedValueLength = timeFormat.replace(':', '').length;
-			var timeMask = new StringMask(timeFormat);
+			var timeFormatVars = {
+				short: {
+					formattedValueLength: '00:00'.length,
+					unformattedValueLength: '00:00'.replace(':', '').length,
+					timeMask: new StringMask('00:00')
+				},
+				long: {
+					formattedValueLength: '00:00:00'.length,
+					unformattedValueLength: '00:00:00'.replace(':', '').length,
+					timeMask: new StringMask('00:00:00')
+				}
+			};
 
 			function formatter(value) {
 				if (ctrl.$isEmpty(value)) {
 					return value;
 				}
+				var cleanValue = value.replace(/[^0-9]/g, '')
+					.slice(0, unformattedValueLength(value.replace(/[^0-9]/g, ''))) || '';
+				return (timeMask(value.replace(/[^0-9]/g, '')).apply(cleanValue) || '')
+					.replace(/[^0-9]$/, '');
+			}
 
-				var cleanValue = value.replace(/[^0-9]/g, '').slice(0, unformattedValueLength) || '';
-				return (timeMask.apply(cleanValue) || '').replace(/[^0-9]$/, '');
+			function getTimeFormat(s, cleanValue) {
+				if (timeFormat) {
+					return timeFormat;
+				} else {
+					if (cleanValue) {
+						if (s.length <= 4) {
+							return 'short';
+						} else {
+							return 'long';
+						}
+					} else {
+						if (s && s.split(':').length >= 3) {
+							return 'long';
+						} else {
+							return 'short';
+						}
+					}
+				}
+			}
+
+			function formattedValueLength(s) {
+				return timeFormatVars[getTimeFormat(s, false)].formattedValueLength;
+			}
+
+			function unformattedValueLength(s) {
+				return timeFormatVars[getTimeFormat(s, true)].unformattedValueLength;
+			}
+
+			function timeMask(s) {
+				return timeFormatVars[getTimeFormat(s, true)].timeMask;
 			}
 
 			ctrl.$formatters.push(formatter);
@@ -42,7 +84,7 @@ module.exports = function TimeMaskDirective() {
 				return modelValue;
 			});
 
-			ctrl.$validators.time = function (modelValue) {
+			ctrl.$validators.time = function(modelValue) {
 				if (ctrl.$isEmpty(modelValue)) {
 					return true;
 				}
@@ -55,7 +97,8 @@ module.exports = function TimeMaskDirective() {
 					minutes = parseInt(splittedValue[1]),
 					seconds = parseInt(splittedValue[2] || 0);
 
-				return modelValue.toString().length === formattedValueLength &&
+				return modelValue.toString().length ===
+					formattedValueLength(modelValue.toString()) &&
 					hours < 24 && minutes < 60 && seconds < 60;
 			};
 		}
