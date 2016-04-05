95949c86034b85394df6e9a081fc9aea8032542f
Diego <diego@vieira.uk>
2016-04-05 07:39:39 -0300

    feat(ui-date-mask): include timezone offset in matching regex
    * refactor(ui-date-mask): regex should match timezone offset
    
    it should match
    0000-00-00T00:00:00.000Z
    0000-00-00T00:00:00.000+00:00
    
    * docs: updated demo to include ui-date-mask

--- src/global/date/date.js
+++ src/global/date/date.js
@@ -4,7 +4,7 @@ var moment = require('moment');
 var StringMask = require('string-mask');
 
 function isISODateString(date) {
-	return /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}Z$/
+	return /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}([-+][0-9]{2}:[0-9]{2}|Z)$/
 		.test(date.toString());
 }
 
