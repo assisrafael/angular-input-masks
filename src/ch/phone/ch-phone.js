d3e4d76bd467eaf5f43297ca019050113fd301a7
Philipp Spinnler <philipp@spinnler.ch>
2016-04-21 15:25:20 +0200

    little clean up

--- src/ch/phone/ch-phone.js
+++ src/ch/phone/ch-phone.js
@@ -4,8 +4,6 @@ var StringMask = require('string-mask');
 var maskFactory = require('mask-factory');
 
 var phoneMask = new StringMask('+00 00 000 00 00');
-	//phoneMask9D = new StringMask('(00) 00000-0000'),
-	//phoneMask0800 = new StringMask('0000-000-0000');
 
 module.exports = maskFactory({
 	clearValue: function(rawValue) {
@@ -13,13 +11,6 @@ module.exports = maskFactory({
 	},
 	format: function(cleanValue) {
 		var formatedValue;
-		/*if (cleanValue.indexOf('0800') === 0) {
-			formatedValue = phoneMask0800.apply(cleanValue);
-		} else if (cleanValue.length < 11) {
-			formatedValue = phoneMask8D.apply(cleanValue) || '';
-		} else {
-			formatedValue = phoneMask9D.apply(cleanValue);
-		}*/
 
 		formatedValue = phoneMask.apply(cleanValue) || '';
 
