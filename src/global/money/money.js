e64da0d6703705dd944ff49e60ab5a125ef98ced
Miguel Carboni <miguel.carboni@gmail.com>
2016-05-26 01:16:25 -0300

    reverting money.js. this is a simple number

--- src/global/money/money.js
+++ src/global/money/money.js
@@ -11,7 +11,6 @@ function MoneyMaskDirective($locale, $parse, PreFormatters) {
 			var decimalDelimiter = $locale.NUMBER_FORMATS.DECIMAL_SEP,
 				thousandsDelimiter = $locale.NUMBER_FORMATS.GROUP_SEP,
 				currencySym = $locale.NUMBER_FORMATS.CURRENCY_SYM,
-				hideCurrencySym = false,
 				decimals = $parse(attrs.uiMoneyMask)(scope);
 
 			function maskFactory(decimals) {
@@ -24,8 +23,6 @@ function MoneyMaskDirective($locale, $parse, PreFormatters) {
 				thousandsDelimiter = '';
 			}
 
-			hideCurrencySym = angular.isDefined(attrs.uiHideCurrencySym);
-			
 			if (isNaN(decimals)) {
 				decimals = 2;
 			}
