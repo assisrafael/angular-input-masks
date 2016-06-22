ef48d89b29eae8369d4d382f9a8610bf5d647fa4
Leonn Leite <leonnleite@gmail.com>
2016-06-22 15:45:51 -0300

    fix in tests

--- src/global/credit-card/credit-card.spec.js
+++ src/global/credit-card/credit-card.spec.js
@@ -59,25 +59,24 @@ describe('ui.utils.masks.number', function() {
 			}
 		});
 
-		it('should apply a phone number mask in a model with default value:', function() {
+		it('should apply a credit card number mask in a model with default value:', function() {
 			var BS = protractor.Key.BACK_SPACE;
 
 			var tests = [
-				{key:'1', viewValue:'+1', modelValue:'1'},
-				{key:'2', viewValue:'+12', modelValue:'12'},
-				{key:'3', viewValue:'+12 3', modelValue:'123'},
-				{key:'4', viewValue:'+12 34', modelValue:'1234'},
-				{key:'5', viewValue:'+12 34 5', modelValue:'12345'},
-				{key:'6', viewValue:'+12 34 56', modelValue:'123456'},
-				{key:'7', viewValue:'+12 34 567', modelValue:'1234567'},
-				{key:BS, viewValue:'+12 34 56', modelValue:'123456'},
-				{key:BS, viewValue:'+12 34 5', modelValue:'12345'},
-				{key:BS, viewValue:'+12 34 ', modelValue:'1234'},
-				{key:BS, viewValue:'+12 34', modelValue:'1234'},
-				{key:BS, viewValue:'+12 3', modelValue:'123'},
-				{key:BS, viewValue:'+12 ', modelValue:'12'},
-				{key:BS, viewValue:'+12', modelValue:'12'},
-				{key:BS, viewValue:'+1', modelValue:'1'},
+				{key:'1', viewValue:'1', modelValue:'1'},
+				{key:'2', viewValue:'12', modelValue:'12'},
+				{key:'3', viewValue:'123', modelValue:'123'},
+				{key:'4', viewValue:'1234', modelValue:'1234'},
+				{key:'5', viewValue:'1234 5', modelValue:'12345'},
+				{key:'6', viewValue:'1234 56', modelValue:'123456'},
+				{key:'7', viewValue:'1234 567', modelValue:'1234567'},
+				{key:BS, viewValue:'1234 56', modelValue:'123456'},
+				{key:BS, viewValue:'1234 5', modelValue:'12345'},
+				{key:BS, viewValue:'1234 ', modelValue:'1234'},
+				{key:BS, viewValue:'1234', modelValue:'1234'},
+				{key:BS, viewValue:'123', modelValue:'123'},
+				{key:BS, viewValue:'12', modelValue:'12'},
+				{key:BS, viewValue:'1', modelValue:'1'},
 				{key:BS, viewValue:'', modelValue:''},
 			];
 
