bcfa4c8a731153c15d5a90c1c0a32aa2d7fc8c68
Richard Lopes <richard.lopes92@gmail.com>
2016-10-12 10:24:37 -0400

    allow typing br-phone-number without DDD, rewriting tests

--- src/br/phone/br-phone.test.js
+++ src/br/phone/br-phone.test.js
@@ -56,11 +56,11 @@ describe('ui-br-phone-number', function() {
 
 		var tests = [
 			{value:'@', viewValue:'', modelValue:''},
-			{value:'2-', viewValue:'(2', modelValue:'2'},
-			{value:'23a', viewValue:'(23', modelValue:'23'},
-			{value:'23_34', viewValue:'(23) 34', modelValue:'2334'},
-			{value:'23346!', viewValue:'(23) 346', modelValue:'23346'},
-			{value:'23346!324', viewValue:'(23) 3463-24', modelValue:'23346324'},
+			{value:'2-', viewValue:'2', modelValue:'2'},
+			{value:'23a', viewValue:'23', modelValue:'23'},
+			{value:'23_34', viewValue:'2334', modelValue:'2334'},
+			{value:'23346!', viewValue:'2334-6', modelValue:'23346'},
+			{value:'23346!324', viewValue:'2334-6324', modelValue:'23346324'}
 		];
 
 		tests.forEach(function(test) {
@@ -89,9 +89,19 @@ describe('ui-br-phone-number', function() {
 		var model = input.controller('ngModel');
 		expect(model.$viewValue).toBe('(12) 3456-7890');
 		expect(model.$modelValue).toBe('1234567890');
+
 		input.val('12345678901').triggerHandler('input');
 		expect(model.$viewValue).toBe('(12) 34567-8901');
 		expect(model.$modelValue).toBe('12345678901');
+
+		input.val('32375486').triggerHandler('input');
+		expect(model.$viewValue).toBe('3237-5486');
+		expect(model.$modelValue).toBe('32375486');
+
+		input.val('992561546').triggerHandler('input');
+		expect(model.$viewValue).toBe('99256-1546');
+		expect(model.$modelValue).toBe('992561546');
+
 		input.val('08001231234').triggerHandler('input');
 		expect(model.$viewValue).toBe('0800-123-1234');
 		expect(model.$modelValue).toBe('08001231234');
