b3f327200d48ddef9571dea3c48b44432a7a0fc1
Igor Rafael <igor.rafael@gmail.com>
2017-09-22 17:55:07 -0300

    chore: improve e2e tests in CI environments

--- config/protractor.conf.js
+++ config/protractor.conf.js
@@ -2,34 +2,45 @@
 
 /*eslint no-process-env: 0*/
 
+const {SpecReporter} = require('jasmine-spec-reporter');
+
 var config = {
+	allScriptsTimeout: 11000,
+	directConnect: true,
+	capabilities: {
+		browserName: 'chrome'
+	},
+	framework: 'jasmine',
+	jasmineNodeOpts: {
+		showColors: true,
+		defaultTimeoutInterval: 30000,
+		print() {}
+	},
 	specs: [
 		'../src/**/*.spec.js'
 	],
-	directConnect: true,
-	framework: 'jasmine',
-	multiCapabilities: [{
-		browserName: 'chrome'
-	}],
 	baseUrl: 'http://localhost:9090/demo',
-	beforeLaunch: function() {
+	beforeLaunch() {
 		var exec = require('child_process').exec;
 
 		exec('gulp serve');
 		return new Promise((resolve) => {
 			setTimeout(resolve, 5000);
 		});
+	},
+	onPrepare() {
+		jasmine.getEnv().addReporter(new SpecReporter({
+			spec: {
+				displayStacktrace: true
+			}
+		}));
 	}
 };
 
-if (process.env.TRAVIS) {
-	config.allScriptsTimeout = 110000;
-	config.jasmineNodeOpts = {
-		defaultTimeoutInterval: 100000
+if (process.env.CI) {
+	config.capabilities.chromeOptions = {
+		args: ['--headless', '--disable-gpu', '--window-size=800x600']
 	};
-	config.multiCapabilities = [{
-		browserName: 'firefox'
-	}];
 }
 
 exports.config = config;
