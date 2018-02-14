50ac3d5cc5169b7e815b4e891c69f4437d5f6dae
Buzunda <luizrobertoavila@gmail.com>
2018-02-14 21:02:26 -0200

    Added a caret to mask factory to fix issues with the cursor positioning in android with chrome. Fix #305

--- src/helpers/mask-factory.js
+++ src/helpers/mask-factory.js
@@ -2,6 +2,46 @@
 
 module.exports = function maskFactory(maskDefinition) {
 	return function MaskDirective() {
+		
+		var caret = {
+			set : function caretSet(element, currentPos, pos) {				
+
+				if (element[0].setSelectionRange) {
+					element[0].focus();
+					window.setTimeout(function() {
+						if(currentPos!=element[0].selectionStart){
+							currentPos=element[0].selectionStart;
+						}
+						pos=currentPos+pos;
+						element[0].setSelectionRange(pos, pos);
+					}, 0);
+					
+
+				} else if (element[0].createTextRange) {
+					pos=currentPos+pos;
+					var range = element[0].createTextRange();
+					range.collapse(true);
+					range.moveEnd('character', pos);
+					range.moveStart('character', pos);
+					range.select();
+				}
+			},
+			get : function caretGet(element) {
+				var iCaretPos = 0;
+				if (document.selection) {
+					element[0].focus();
+					var oSel = document.selection.createRange();
+					oSel.moveStart('character', -element[0].value.length);
+					iCaretPos = oSel.text.length;
+				} else if (
+					element[0].selectionStart || element[0].selectionStart === '0'
+				) {
+					iCaretPos = element[0].selectionStart;
+				}
+				return iCaretPos;
+			}
+		};
+
 		return {
 			restrict: 'A',
 			require: 'ngModel',
@@ -11,7 +51,7 @@ module.exports = function maskFactory(maskDefinition) {
 						return value;
 					}
 
-					var cleanValue = maskDefinition.clearValue(value.toString());
+					var cleanValue = maskDefinition.clearValue(value);
 					return maskDefinition.format(cleanValue);
 				});
 
@@ -20,20 +60,36 @@ module.exports = function maskFactory(maskDefinition) {
 						return value;
 					}
 
-					var cleanValue = maskDefinition.clearValue(value.toString());
-					var formattedValue = maskDefinition.format(cleanValue);
 
-					if (ctrl.$viewValue !== formattedValue) {
-						ctrl.$setViewValue(formattedValue);
-						ctrl.$render();
-					}
+						var cleanValue = maskDefinition.clearValue(value);
+						var formattedValue = maskDefinition.format(cleanValue);
 
-					if (angular.isUndefined(maskDefinition.getModelValue)) {
-						return cleanValue;
-					}
+						var currentCaretPosition = caret.get(element),
+							lengthBefore = ctrl.$viewValue.length,
+							lengthAfter = 0;
 
-					var actualModelType = typeof ctrl.$modelValue;
-					return maskDefinition.getModelValue(formattedValue, actualModelType);
+						if (ctrl.$viewValue !== formattedValue) {
+							ctrl.$setViewValue(formattedValue);
+							ctrl.$render();
+
+							lengthAfter = ctrl.$viewValue.length;
+						}
+
+						var caretFormatDiffer = (lengthAfter - lengthBefore) < 0
+												? 0
+												: lengthAfter - lengthBefore;
+
+						caret.set(element, currentCaretPosition, caretFormatDiffer);
+
+						if (angular.isUndefined(maskDefinition.getModelValue)) {
+							return cleanValue;
+						}
+
+						var actualModelType = typeof ctrl.$modelValue;
+						return maskDefinition.getModelValue(formattedValue, actualModelType);
+				
+
+					
 				});
 
 				angular.forEach(maskDefinition.validations, function(validatorFn, validationErrorKey) {
