b55a9fccc9f0bd56942881fda01094b2fdcaeb0a
José Albino Júnior <jose.albino.junior@accenture.com>
2018-10-23 17:57:02 -0300

    fix caret jumps to end of input

--- src/helpers/mask-factory.js
+++ src/helpers/mask-factory.js
@@ -2,10 +2,57 @@
 
 module.exports = function maskFactory(maskDefinition) {
 	return function MaskDirective() {
+		var isBackspace = false;
+		var caretCtrl = {
+			get: function getCaretPosition(element) {
+				var caretPosition = 0;
+				if (document.selection) {
+					element.focus();
+					var range = document.selection.createRange();
+					range.moveStart('character', -element.value.length);
+					caretPosition = range.text.length;
+				} else if (element.selectionStart || element.selectionStart == '0') {
+					caretPosition = element.selectionStart;
+				}
+				return caretPosition;
+			},
+			set: function setCaretPosition(element, position) {
+				
+				if (/[^0-9a-z]/gi.test(element.value.charAt(position - 1))
+					&& element.value.charAt(position + 1) === null && !isBackspace) {
+					position += /\s/.test(element.value.charAt(position)) ? 3 : 2;
+				} else if (/[^0-9a-z]/gi.test(element.value.charAt(position - 1))
+					&& element.value.charAt(position + 1) !== null && !isBackspace) {
+					position += /\s/.test(element.value.charAt(position)) ? 2 : 1;
+				}
+
+				if(element.setSelectionRange) {
+					element.focus();
+					setTimeout(function () {
+						element.setSelectionRange(position, position);
+					}, 0);
+				} else if (element.createTextRange) {
+					var range = element.createTextRange();
+					range.collapse(true);
+					range.moveEnd('character', position);
+					range.moveStart('character', position);
+					range.select();
+				}
+			}
+		}
 		return {
 			restrict: 'A',
 			require: 'ngModel',
 			link: function(scope, element, attrs, ctrl) {
+
+				element.on('keydown', function (event) {
+					if(event.keyCode == '8' || event.keyCode == '46'){
+						isBackspace = true;
+					} else {
+						isBackspace = false;
+					}
+				});
+				
 				ctrl.$formatters.push(function formatter(value) {
 					if (ctrl.$isEmpty(value)) {
 						return value;
@@ -23,10 +70,14 @@ module.exports = function maskFactory(maskDefinition) {
 					var cleanValue = maskDefinition.clearValue(value.toString(), attrs);
 					var formattedValue = maskDefinition.format(cleanValue);
 
+					var currentPosition = caretCtrl.get(element[0]);
+
 					if (ctrl.$viewValue !== formattedValue) {
 						ctrl.$setViewValue(formattedValue);
 						ctrl.$render();
 					}
+
+					caretCtrl.set(element[0], currentPosition);
 
 					if (angular.isUndefined(maskDefinition.getModelValue)) {
 						return cleanValue;
