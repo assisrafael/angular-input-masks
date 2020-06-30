18422591fb852d53534a0432929cda3b3d82f8b9
condereis <rafael.lcreis@gmail.com>
2020-06-30 17:17:06 -0300

    Add mask for American Express cards

--- src/global/credit-card/credit-card.js
+++ src/global/credit-card/credit-card.js
@@ -3,25 +3,32 @@
 var StringMask = require('string-mask');
 var maskFactory = require('../../helpers/mask-factory');
 
-var ccSize = 16;
+var defaultSize = 16;
+var amexSize = 15;
 
-var ccMask = new StringMask('0000 0000 0000 0000');
+var defaultMask = new StringMask('0000 0000 0000 0000');
+var amexMask = new StringMask('0000 000000 00000');
+
 
 module.exports = maskFactory({
 	clearValue: function(rawValue) {
-		return rawValue.toString().replace(/[^0-9]/g, '').slice(0, ccSize);
+		return rawValue.toString().replace(/[^0-9]/g, '').slice(0, defaultSize);
 	},
 	format: function(cleanValue) {
 		var formatedValue;
 
-		formatedValue = ccMask.apply(cleanValue) || '';
+		if (cleanValue.startsWith("34") || cleanValue.startsWith("37")) {
+			formatedValue = amexMask.apply(cleanValue) || '';
+		} else {
+			formatedValue = defaultMask.apply(cleanValue) || '';
+		}
 
 		return formatedValue.trim().replace(/[^0-9]$/, '');
 	},
 	validations: {
 		creditCard: function(value) {
 			var valueLength = value && value.toString().length;
-			return valueLength === ccSize;
+			return valueLength === defaultSize || valueLength === amexSize;
 		}
 	}
 });
